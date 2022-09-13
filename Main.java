3cc1d1b46a7eca419afead216848accc8ad78188
Mr. Kaehms <kaehmsrobert@dublinusd.org>
2022-09-13 21:37:11 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    TestClass t=new TestClass();
+    t.printMyName();
+  }
+}
+/** sample code
+class Main {
+	public static void main(String[] args) {
+		Adder adder = new Adder();
+		System.out.println(adder.add(11, 31));
+		System.out.println(adder.add(1.5, 3.0));
+		System.out.println(adder.add("Hello ", "World"));
+	}
+}
+**/
---
+++ TestClass.java
@@ -0,0 +1,9 @@
+class TestClass{
+  public String myName;
+public TestClass(){
+myName="TestClass";
+}
+public void printMyName(){
+ System.out.println(myName);
+}
+}
